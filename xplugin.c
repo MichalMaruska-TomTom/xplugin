0ab9882ac5413f3ce6d206a8152fce3bf3e98fba
Michal Maruska <mmaruska@gmail.com>
2010-05-30 17:22:54 +0000

    init

---
+++ xplugin.c
@@ -0,0 +1,109 @@
+
+/* fixme: This should be  gcc -DMMC_PIPELINE  by  xmkmf/imake!  -D TOP*/
+#define MMC_PIPELINE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <X11/XKBlib.h>
+#include <X11/keysym.h>
+#include <X11/extensions/XKB.h>
+#include <X11/Xatom.h>
+
+#include <X11/extensions/XKBstr.h>
+#include <X11/XKBlib.h>
+/* -Dxkb_plugin_dir */
+/* #define XKB_PLUGIN_DIR "{top-x-directory}/lib/modules/xkb-plugins/" */
+
+int
+main(int argc, char** argv)
+{
+    char* cname = (char*)0;
+    /* fixme! */
+    int major=1;
+    int minor=0;
+    int error_rtrn;
+    int event_rtrn;
+    int reason_rtrn;
+    int device = XkbUseCoreKbd;
+    
+    Display* dpy;                 /* cname */
+
+    int res;
+    int i = 1;
+   
+    if (argc<2){
+	printf("usage: xplugin -d {number} [-]filename\nxplugin -vl\n  where filename is in the %s\n",
+	       "XKB_PLUGIN_DIR");
+	exit(-1);
+    }
+    //  init the XKB & X
+    dpy = XkbOpenDisplay(getenv("DISPLAY"), &event_rtrn, &error_rtrn, &major, &minor, &reason_rtrn);
+   
+    if (dpy == NULL) {
+	fprintf(stderr, "XkbOpenDisplay on %s failed: %d %s\n", getenv("DISPLAY"),
+		reason_rtrn,
+		(reason_rtrn == XkbOD_BadLibraryVersion)?"version":"??");
+	perror ("XkbOpenDisplay");
+	exit (-1);
+    }
+
+    if (0 == strcmp(argv[i],"-d")) {
+	i++;
+	device = atoi(argv[i++]);
+    }
+    
+    if (0 == strcmp(argv[i],"-v"))
+	{
+	    printf("dpy: xkb version: %d.%d reason: %d\n", major, minor, reason_rtrn);
+	    i++;
+	}
+    else if (0 == strcmp(argv[i],"-l"))
+	{
+	    XkbPipelineListPtr p = XkbListPipeline(dpy, device);
+	    i++;
+	    if (!p)
+		exit(-1);
+
+	    printf("%d plugin%s in the pipeline\n",
+		   p->num_plugins,
+		   (p->num_plugins==0)?"":"s");
+	    int i;
+	    for(i= 0; i < p->num_plugins; i++)
+		{                       /* flags ? */
+		    printf("%d: %s\n", p->plugins[i].id, p->plugins[i].name);
+		}
+	} else {
+	if (argv[i][0] == '-') {
+	    printf ("removing %s\n", argv[i]);
+	    res = XkbSetPlugin(dpy, device, "", argv[i] +1, True); /* before */
+	    if (res == False)
+		printf ("XkbSetPlugin failed\n");
+	} else {
+	    /* Check if it's in! */
+	    /* XGrabServer(dpy); */
+	    char* module_name = argv[i];
+	    char* in_front_of = "xkb-auto-repeat";
+      
+	    XkbPipelineListPtr p = XkbListPipeline(dpy, device);
+	    /* Is it already loaded? */
+	    int i;
+	    for(i= 0; i < p->num_plugins; i++)
+		if (0 == strcmp(module_name, p->plugins[i].name))
+		    {
+			printf ("%s already loaded\n", module_name);
+			goto close;
+		    }
+	    /* i might test, if in_front_of is present! */
+	    printf ("loading %s\n", module_name);
+	    res = XkbSetPlugin(dpy, device, module_name, in_front_of, True); /* before */
+	    /* XUngrabServer(dpy); */
+	    if (res == False)
+		printf ("XkbSetPlugin failed\n");
+	}
+    }
+  close:
+    XCloseDisplay(dpy);
+    exit ((res == True)?0:1);
+}
